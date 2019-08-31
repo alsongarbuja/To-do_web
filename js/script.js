7a34ad6b16b8b0ef9584e802d5915f4da70008b6
Arex23 <garbuja12alson@gmail.com>
2019-08-31 20:51:48 +0545

    added skeleton code for the project

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var tasks = [];
+let plus = document.querySelector(".fas");
+let add = document.querySelector("#task");
+
+let uCompleted = document.querySelector(".uList");
+let Completed = document.querySelector(".CList");
