0c40001d18b3b9e91c9950c18beff2d4ff884a2b
Alson33 <garbuja12alson@gmail.com>
2020-12-20 11:10:46 +0545

    added the basic feauters from 2019

--- js/script.js
+++ js/script.js
@@ -1,54 +1,73 @@
 var tasks = [];
-var list = document.querySelector("#task");
+var listInput = document.querySelector("#task");
 
-var uCompleted = document.querySelector("#uList");
-var Completed = document.querySelector("#cList");
+var uCompleted = document.querySelector(".uncompleted");
+var Completed = document.querySelector(".completed");
+var allList = document.querySelector(".All");
+var unCompletedUlList = document.querySelector("#uList");
+var completedUlList = document.querySelector("#cList");
+var allListUl = document.querySelector("#aList");
+var divisions = document.getElementsByClassName("lists_division");
 
 
+function removeActiveDiv(){
+    for(var i = 0; i < divisions.length; i++){
+        divisions[i].classList.remove("active_division");
+    }
+}
 /* add the task to the task array and call function render() when the plus is clicked*/
 function add()
 {
     // the code runs only if the input has some values or text in it
-    if(list.value)
+    if(listInput.value)
     {
         tasks.push(
             {
-                name: list.value,
+                name: listInput.value,
                 isComplete: false
             }
         );
             
         // after the list is pushed in the array then the input will be erased itself
-        list.value = "";
+        listInput.value = "";
         render();
-        list.focus();
+        listInput.focus();
+    }
+};
+
+document.onkeyup = function(e) {
+    // console.log(e.keyCode);
+    if (e.key == '+') {
+        listInput.focus();
+    }else if (e.altKey && e.key == 'u') {
+        removeActiveDiv();
+        uCompleted.classList.add("active_division");
+        activeIndex  = 0;
+    }else if(e.altKey && e.key == 'c') {
+        removeActiveDiv();
+        Completed.classList.add("active_division");
+        activeIndex = 1;
+    }else if(e.altKey && e.key == 'a') {
+        removeActiveDiv();
+        allList.classList.add("active_division");
+        activeIndex = 2;
     }
 };
 
 
-/*
-* Its completed now
-? how to focus in a input filed with keydown event
-= use the .focus()function,
- */
-// it focus on the input filed when pressed the + button
-document.body.addEventListener("keyup", function(e){
-    if(e.keyCode == 107){
-        list.focus();
-    }
-}, false);
-
-
-/*
- ! Do not change this code
-*/
 /*add the task to the array and call the add() function when enter is hit in input */
-list.addEventListener("keydown", function(event){
-    console.log(event.keyCode);
-    if(event.keyCode == 13){
+listInput.addEventListener("keydown", function(event){
+    if(event.key == 'Enter'){
         add();
-    }
+    } 
 });
+
+listInput.onkeyup = function(event){
+    if(event.altKey && event.key == '-'){
+        listInput.value = "";
+        listInput.blur();
+    }
+}
 
 /*it adds the task in the respective list */
 function render()
@@ -59,23 +78,73 @@ function render()
             function(t, ind)
             {
                 if(t.isComplete){
-                    complete += "<li><input type = 'checkbox' onclick = 'changeStatus(tasks["+ind+"]); render();' checked>"+t.name+"</li><br/>";
+                    complete += "<li><input type = 'checkbox' onclick = 'changeStatus("+ind+");' checked>"+t.name+"</li><br/>";
                 }else{
-                    uncomplete += "<li><input type = 'checkbox' onclick = 'changeStatus(tasks["+ind+"]); render();'>"+t.name+"</li><br/>";
+                    uncomplete += "<li><input type = 'checkbox' onclick = 'changeStatus("+ind+");'>"+t.name+"</li><br/>";
                 }
             }
         )
 
-        uCompleted.innerHTML = uncomplete;
-        Completed.innerHTML = complete;
+        unCompletedUlList.innerHTML = uncomplete;
+        completedUlList.innerHTML = complete;
+
+    addTask();
 };
 
+function addTask(){
+    var isChecked = "";
+    var all = "";
+
+    tasks.forEach(
+        function(t, ind){
+            if(t.isComplete){
+                isChecked = "checked";
+            }else{
+                isChecked = "";
+            }
+            all += "<li><input type='checkbox' onclick='changeStatus("+ind+");' "+isChecked+" >"+t.name+"</li><br/>";
+        }
+    );
+
+    allListUl.innerHTML = all;
+}
+
 /*it changes the stauts that is isComplete of the task */
-function changeStatus(task1)
+function changeStatus(ind)
 {
-    if(task1.isComplete){
-        task1.isComplete = false;
+    if(tasks[ind].isComplete){
+        tasks[ind].isComplete = false;
     }else{
-        task1.isComplete = true;
+        tasks[ind].isComplete = true;
     }
+
+    render();
 };
+
+var activeIndex = 0;
+
+function slideDivision(pressedBtn){
+    
+    removeActiveDiv();
+
+    activeIndex += pressedBtn;
+
+    if(activeIndex > 2){
+        activeIndex = 0;
+    }
+
+    if(activeIndex < 0){
+        activeIndex = 2;
+    }
+
+    divisions[activeIndex].classList.add("active_division");
+
+}
+
+document.getElementsByClassName("left-arrow")[0].addEventListener('click', function(){
+    slideDivision(-1);
+});
+
+document.getElementsByClassName("right-arrow")[0].addEventListener('click', function(){
+    slideDivision(1);
+});
