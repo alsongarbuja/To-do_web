cc5566eb9827c234b2c441c642c8f7d43eb7d822
alsongarbuja <garbuja12alson@gmail.com>
2023-03-06 15:59:53 +0545

    add: css and util add

---
+++ src/utils/keybinds.ts
@@ -0,0 +1,52 @@
+export const keybinds = [
+    {
+        id: 0,
+        bind: '+',
+        function: 'to start inputing TODO',
+    },
+    {
+        id: 1,
+        bind: 'Alt + -',
+        function: 'to cancel the input',
+    },
+    {
+        id: 2,
+        bind: 'enter',
+        function: 'to add the TODO',
+    },
+    {
+        id: 3,
+        bind: 'Alt + u',
+        function: 'to see the uncompleted TODOs',
+    },
+    {
+        id: 4,
+        bind: 'Alt + c',
+        function: 'to see the completed TODOs',
+    },
+    {
+        id: 5,
+        bind: 'Alt + a',
+        function: 'to see the all TODOs',
+    },
+    {
+        id: 6,
+        bind: 'Alt + t',
+        function: 'to see the deleted TODOs',
+    },
+    {
+        id: 7,
+        bind: 'Left and Right arrows',
+        function: 'to change the lists',
+    },
+    {
+        id: 8,
+        bind: 'Alt + s',
+        function: 'to select the first task of the list.',
+    }, 
+    {
+        id: 9,
+        bind: 'Up and  Down arrows',
+        function: 'to navigate the task in list',
+    }
+];
