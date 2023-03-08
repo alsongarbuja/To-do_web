62cd184d5bb72b21ce07276646e6d1846ff2a154
alsongarbuja <garbuja12alson@gmail.com>
2023-03-08 10:05:04 +0545

    WIP: creating task cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
+import TaskCard from "./components/UI/TaskCard"
 import MasterLayout from "./layouts/MasterLayout"
 
 const App = () => {
   return (
     <div className="w-screen h-screen bg-[#252525]">
       <MasterLayout>
-        {
-          // This is the content that will be rendered in the MasterLayout
-        }
+        <TaskCard />
       </MasterLayout>
     </div>
   )
